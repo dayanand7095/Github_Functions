c9982ab8af4ea5e3e0d06a168dac8f49cfa763a9
“dayanand7095” <dayanand.raj1231997@gmail.com>
2025-12-05 19:24:10 +0530

    Daya3 Commit

--- Demo.java
+++ Demo.java
@@ -1,9 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Demo{
 
     public static void main(String args[]){
         System.out.println("hello world");
         String value="Dayanand";
         System.out.println(value.length());
-
+        Map<Character, Integer> ch=new HashMap<>();
+        
     }
 }
